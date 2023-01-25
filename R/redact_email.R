02966a5300c5ad65db65730ed5591498b002d73f
Caesar Wong <caesar.wongw@gmail.com>
2023-01-25 09:48:18 -0800

    adding package prefix when calling function

--- R/redact_email.R
+++ R/redact_email.R
@@ -22,7 +22,7 @@ redact_email <- function(string, hash_spotted=FALSE, replace_with="EMAILADDRS") 
 
   regex <- "\\b[a-z0-9!#$%&'*+/=?^_`{|}~-](?:[\\.a-z0-9!#$%&'*+/=?^_`{|}~-]{0,62}[a-z0-9!#$%&'*+/=?^_`{|}~-])?(?:@|\\sat\\s)[a-z0-9](?:[a-z0-9-]+(\\.|\\sdot\\s)(?:\\.|\\sdot\\s|[a-z0-9-]){0,251}[a-z0-9])+\\b"
   if (hash_spotted) {
-    new_text <- str_replace_all(string, regex, function(x) md5(x[1]))
+    new_text <- stringr::str_replace_all(string, regex, function(x) openssl::md5(x[1]))
     return(new_text)
   } else {
     new_text <- gsub(regex, replace_with, string, ignore.case = TRUE)
