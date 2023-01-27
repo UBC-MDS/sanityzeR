6441d55bdca53aa0a1e846cc2ced98f1f47ad5e6
Jonah Hamilton <jonah.hamilton@gmail.com>
2023-01-27 12:33:49 -0800

    edit tests for end-to-end tests

--- R/redact_creditcardnumber.R
+++ R/redact_creditcardnumber.R
@@ -22,14 +22,14 @@ redact_creditcardnumber <- function(string, hash_spotted=FALSE, replace_with="CR
   regex <- "(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|(222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{13}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\\d{3})\\d{11})"
 
   # removes and white space or hyphens
-  string <- stringr::str_replace_all(string, "[-\\s]", "")
+  clean_string <- stringr::str_replace_all(string, "[-\\s]", "")
 
   if (hash_spotted) {
-    new_text <- stringr::str_replace_all(string, regex, function(x) openssl::md5(x[1]))
-    return(new_text)
+    new_clean_text <- stringr::str_replace_all(clean_string, regex, function(x) openssl::md5(x[1]))
+    return(new_clean_text)
   } else {
-    new_text <- stringr::str_replace_all(string, regex, replace_with)
-    return(new_text)
+    new_clean_text <- stringr::str_replace_all(clean_string, regex, replace_with)
+    return(new_clean_text)
   }
 }
 
