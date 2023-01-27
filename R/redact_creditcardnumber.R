7c994d01989c01c1d95863e5f1a5602bec41cbf4
Jonah Hamilton <jonah.hamilton@gmail.com>
2023-01-27 11:11:54 -0800

    add function body to redact credit card

--- R/redact_creditcardnumber.R
+++ R/redact_creditcardnumber.R
@@ -12,8 +12,25 @@
 #' x <- "You can use my 5567554868135971 here"
 #' redact_creditcardnumber(x)
 redact_creditcardnumber <- function(string, hash_spotted=FALSE, replace_with="CREDITCARD") {
+
   # to be implemented in the next milestone
   print(string)
   print(hash_spotted)
   print(replace_with)
+
+  # regex pattern matches: Visa - Mastercard - Amex - Discover - Diners Club - JCB
+  regex <- "(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|(222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{13}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\\d{3})\\d{11})"
+
+  # removes and white space or hyphens
+  string <- str_replace_all(fruits, "[-\\s]", "")
+
+  if (hash_spotted) {
+    new_text <- stringr::str_replace_all(string, regex, function(x) openssl::md5(x[1]))
+    return(new_text)
+  } else {
+    new_text <- stringr::str_replace_all(string, regex, replace_with)
+    return(new_text)
+  }
 }
+
+
