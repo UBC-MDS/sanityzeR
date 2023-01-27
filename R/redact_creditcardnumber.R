fcf602741dd2b7d5664311e77107302c26e10864
Jonah Hamilton <jonah.hamilton@gmail.com>
2023-01-27 11:15:52 -0800

    fix stringer import - redact credit card

--- R/redact_creditcardnumber.R
+++ R/redact_creditcardnumber.R
@@ -22,7 +22,7 @@ redact_creditcardnumber <- function(string, hash_spotted=FALSE, replace_with="CR
   regex <- "(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|(222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{13}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\\d{3})\\d{11})"
 
   # removes and white space or hyphens
-  string <- str_replace_all(fruits, "[-\\s]", "")
+  string <- stringr::str_replace_all(string, "[-\\s]", "")
 
   if (hash_spotted) {
     new_text <- stringr::str_replace_all(string, regex, function(x) openssl::md5(x[1]))
